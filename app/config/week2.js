591816ad203849f269fda16e141fdfbddca7f6ca
McSick <michael.c.sickles@gmail.com>
2015-09-16 19:35:51 -0400

    V2

---
+++ app/config/week2.js
@@ -0,0 +1,86 @@
+week2 = [
+{
+'id':0,
+  'Away':'Broncos',
+  'Home':'Chiefs'
+
+
+},
+{
+'id':1,
+  'Away':'Texans',
+  'Home':'Panthers'
+
+},{
+'id':2,
+  'Away':'49ers',
+  'Home':'Steelers'
+
+},{
+'id':3,
+  'Away':'Buccaneers',
+  'Home':'Saints'
+
+},{
+'id':4,
+  'Away':'Lions',
+  'Home':'Vikings'
+
+},{
+'id':5,
+  'Away':'Cardinals',
+  'Home':'Bears'
+
+},{'id':6,
+  'Away':'Patriots',
+  'Home':'Bills'
+
+},{
+'id':7,
+  'Away':'Chargers',
+  'Home':'Bengals'
+
+},{
+'id':8,
+  'Away':'Titans',
+  'Home':'Browns'
+
+},{
+'id':9,
+  'Away':'Falcons',
+  'Home':'Giants'
+
+},{
+'id':10,
+  'Away':'Rams',
+  'Home':'Redskins'
+
+},{
+'id':11,
+  'Away':'Dolphins',
+  'Home':'Jaguars'
+
+},{
+'id':12,
+  'Away':'Ravens',
+  'Home':'Raiders'
+
+},{
+'id':13,
+  'Away':'Cowboys',
+  'Home':'Eagles'
+
+},{
+'id':14,
+  'Away':'Seahawks',
+  'Home':'Packers'
+
+},{
+'id':15,
+  'Away':'Jets',
+  'Home':'Colts'
+
+}
+
+
+];
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,26 +1,29 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'firebase'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+.config(['$routeProvider', function ($routeProvider) {
+			$routeProvider.when('/view1', {
+				templateUrl : 'view1/view1.html',
+				controller : 'View1Ctrl'
+			});
+		}
+	])
 
-.controller('View1Ctrl', function($scope) {
-
-
-	$scope.games = week1;
+.controller('View1Ctrl', function ($scope, $firebaseObject) {
+	var ref = new Firebase("https://nflquickpick.firebaseio.com");
+	$scope.games = angular.copy(week2);
+	$scope.name = '';
+	// download the data into a local object
+	$scope.data = $firebaseObject(ref);
 	$scope.winners = [];
-	$scope.points = [99,75,50,40,30,20,10,9,8,7,6,5,4,3,2,1];
+	$scope.points = [99, 75, 50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 	$scope.pointchoice = [];
 	$scope.picks = [];
-	$scope.init = function(){
-		for(var i =0; i < week1.length; i++){
+	$scope.init = function () {
+		for (var i = 0; i < week2.length; i++) {
 			var pick = {
-				team : week1[i].Away,
+				team : week2[i].Away,
 				points : ''
 			}
 			$scope.winners[i] = pick;
@@ -28,25 +31,64 @@ angular.module('myApp.view1', ['ngRoute'])
 		}
 
 	}
-	$scope.addGame = function(index){
-		console.log(index + ' game - ' + $scope.winners[index] + ' ' + $scope.winners[index].points);
-		$scope.picks.push({
-			winner:$scope.winners[index].team,
-			points:$scope.winners[index].points
+	$scope.savedPicks = [];
+	$scope.getPicks = function () {
+		var serverPicks = $firebaseObject(ref.child('week2').child($scope.name));
+		serverPicks.$loaded(function (savedPicks) {
+
+			savedPicks.forEach(function (value, key) {
+				if (!angular.isFunction(value)) {
+					$scope.picks.push({
+						winner : value,
+						points : key
+
+					});
+				}
+			});
 		});
-
-
-
-		$scope.points.splice($scope.points.indexOf($scope.winners[index].points),1);
-		$scope.winners.splice(index,1);
-		$scope.games.splice(index,1);
-		
-
-		
+		$scope.picks.sort(function(a,b){
+			return parseInt(b.points) - parseInt(a.points);
+		});
+		$scope.games = [];
+		$scope.points = [];
 
 	};
-	
-	
+	$scope.submit = function () {
+		$scope.savedPicks = $firebaseObject(ref.child('week2').child($scope.name));
+		for (var i = 0; i < $scope.picks.length; i++) {
+			var pick = $scope.picks[i];
+			$scope.savedPicks[pick.points] = pick.winner;
+		}
+		$scope.savedPicks.$save();
+
+	};
+
+	$scope.addGame = function (index) {
+		console.log(index + ' game - ' + $scope.winners[index] + ' ' + $scope.winners[index].points);
+		$scope.picks.push({
+			winner : $scope.winners[index].team,
+			points : $scope.winners[index].points
+		});
+
+		$scope.points.splice($scope.points.indexOf($scope.winners[index].points), 1);
+		for (var i = $scope.games.length - 1; i >= 0; i--) {
+			if ($scope.games[i].id == index)
+				$scope.games.splice(i, 1);
+		}
+
+	};
+	$scope.removeGame = function (index) {
+		var pick = $scope.picks[index];
+		for (var i = 0; i < week2.length; i++) {
+			if (week2[i].Home === pick.winner || week2[i].Away === pick.winner) {
+				$scope.games.push(week2[i]);
+				$scope.points.push(pick.points);
+			}
+		}
+		$scope.picks.splice(index, 1);
+
+	};
+
 	$scope.init();
-	
+
 });
