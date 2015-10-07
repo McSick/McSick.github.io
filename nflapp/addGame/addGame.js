83782605129dc0342b8c4090573c2a72ef2e4da4
McSick <michael.c.sickles@gmail.com>
2015-10-07 18:27:00 -0400

    Update addGame.js

--- nflapp/addGame/addGame.js
+++ nflapp/addGame/addGame.js
@@ -13,7 +13,7 @@ angular.module('myApp.addGame', ['ngRoute','firebase'])
 	var ref = new Firebase("https://nflquickpick.firebaseio.com");
 
 	$scope.games = [];
-	$scope.gamenumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+	$scope.gamenumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
 	$scope.totals = [];
 
 	$scope.submit = function(){
